8c2c934bcd03eafbb19a0fa7c7f97bb9b1e3a7f6
Diego <148901730+ddcsoftdev@users.noreply.github.com>
2024-01-18 10:31:17 +0100

    Update MShooterEnemy.h

--- Source/MiniShooter/Enemy/MShooterEnemy.h
+++ Source/MiniShooter/Enemy/MShooterEnemy.h
@@ -38,36 +38,48 @@ public:
 
 	/**
 	* Inflicts Damage
+ 	*
+  	* @param Amount Value that subtracts to current Health
 	*/
 	UFUNCTION()
 		virtual void TakeDamageAmount(float Amount) override;
 
 	/**
 	* Register belonging Patrol Zone
+ 	*
+  	* @return Actor that points to the Patrol Zone or nullptr
 	*/
 	UFUNCTION()
 		AActor* GetActivePatrolZone() { return ActivePatrolZone; }
 
 	/**
 	* Register belonging Patrol Zone
+ 	*
+  	* @param PatrolZone Actor that is set as active Patrol Zone
 	*/
 	UFUNCTION()
 		void RegisterPatrolZone(AActor* PatrolZone);
 
 	/**
 	* Unregister belonging Patrol Zone
+ 	*
+  	* @param PatrolZone Actor which is unreferenced as the active Patrol Zone
 	*/
 	UFUNCTION()
 		void UnRegisterPatrolZone(AActor* PatrolZone);
 
 	/**
 	* Updates TargetLocation for AI
+ 	*
+  	* @param PatrolPoint Actor that updates the AI Component TargetLocation
 	*/
 	UFUNCTION()
 		void UpdateAITargetLocation(AActor* PatrolPoint);
 
 	/**
 	* Toggels on and off the AI
+ 	*
+  	* @param bActivate Value that activates or deactivates the AI Component
 	*/
 	UFUNCTION()
 		void SetAIBehaviour(bool bActivate);
@@ -130,19 +142,22 @@ protected:
 	* Internal update health bar widget
 	* It uses Health and MaxHealth Attributes from class
 	* 
+ 	* @param MaxHealth Value that represents max health of the Actor
+  	* @param CurrentHealth Value that represents the current health of the Actor
 	*/
 	UFUNCTION()
 		virtual void UpdateHealthBarWidget(float MaxHealth, float CurrentHealth) override;
 
 	/**
 	* Rotate Healthbar
-	* 
 	*/
 	UFUNCTION()
-	virtual void SetHealthWidgetRotation() override;
+		virtual void SetHealthWidgetRotation() override;
 
 	/**
 	* Comunicates AI Component about player within zone
+ 	*
+  	* @param bIsInsideZone Value that represents if Player is within ActivePatrolZone
 	*/
 	UFUNCTION()
 		void HandlePlayerWithinZone(bool bIsInsideZone);
